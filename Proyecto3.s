e4a48a076d920bd7fe4adfcdb8aff7d8d642899e
Juan De Leon <dele17822@uvg.edu.gt>
2019-04-25 01:31:42 -0600

    Multiplayer finalizado

--- Proyecto3.s
+++ Proyecto3.s
@@ -10,13 +10,33 @@
 
 /*Variables*/
 opcion_de_juego: .word 0
+opcion_mayor_menor: .word 0
 
 /*Mensajes*/
-mensaje_bienvenida: .asciz "\n\t\tBIENVENIDO\n\n\tJUEGO MAYOR O MENOR\n\n1. Single Player\n2. Multiplayer"
-mensaje_ingreso: .asciz "\nIngrese una opcion:"
+mensaje_bienvenida: .asciz "\n\t\tBIENVENIDO\n\n\tJUEGO MAYOR O MENOR\n\nModos de juego:\n\n\t1. Single Player\n\t2. Multiplayer"
+mensaje_ingreso: .asciz "\nIngrese una opcion: "
 ingreso_valor: .asciz "%d"
 mensaje_error: .asciz "\nEl valor ingresado no es valido\n"
+mensaje_mayor_menor: .asciz "\nEl sigiente valor sera MENOR(0) O MAYOR(1):"
 
+/*Mensajes turnos*/
+turno_jugador1: .asciz "\n\t\tTURNO JUGADOR 1"
+turno_jugador2: .asciz "\n\t\tTURNO JUGADOR 2"
+turno_computadora: .asciz "\nTURNO COMPUTADORA"
+
+/*Menajes de dados*/
+mensaje_dados: .asciz "\n-------------VALOR DE DADOS: %ld---------------\n"
+mensaje_dados1: .asciz "\nDado 1: %d\n"
+mensaje_dados2: .asciz "Dado 2: %d\n"
+
+/*Mensaje Marcadores*/
+mensaje_marcador: .asciz "\n\*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n\n\t\tMARCADOR\n"
+marcador_jugador1: .asciz ">>>JUGADOR 1: %d\n"
+marcador_jugador2: .asciz ">>>JUGADOR 2: %d\n"
+
+/*Mensajes victorias*/
+jugador1_gano: .asciz "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\t\tJUAGODR 1 HA GANADO\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+jugador2_gano: .asciz "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\t\tJUAGODR 2 HA GANADO\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
 .text
 .align 2
@@ -33,10 +53,12 @@ main:
 	/*Modo de juego*/
 	BL modo_juego
 
+	B fin
+
 
 modo_juego:
 
-	MOV r10, lr
+	PUSH {lr}					@Guarda la instruccion donde fue llamada la subrutina
 
 	/*Ingreso de opcion de juego*/
 	LDR r0, =mensaje_ingreso			@Mensaje de ingreso
@@ -52,8 +74,10 @@ modo_juego:
 	LDR r1, =opcion_de_juego			@Valor ingresado
 	LDR r1, [r1]
 
-	MOV r11, #0					/*R11 contendra el turno del jugador*/
-	MOV r12, #0					/*R12 sera el inicio del juego*/
+	MOV r10, #0					/*R10 contendra el turno del jugador*/
+	MOV r11, #0					/*R11 sera el inicio del juego*/
+	MOV r7, #0					/*R7 sera el marcador del jugador1*/
+	MOV r8, #0					/*R8 sera el marcador del jugador 2*/
 
 	CMP r1, #1					@En caso ser 1 se llama a single player
 	BEQ single_player
@@ -61,32 +85,262 @@ modo_juego:
 	CMP r1, #2					@En caso ser 2 se llama a multiplayer
 	BEQ multiplayer
 
-	B error						@En caso el numero ingresado no es valido
+	BGT error					@En caso el numero ingresado no es valido
+
+	POP {lr}					@Devuelve a donde fue llamada la subrutina
 
 
 single_player:
 	/*Jugar contra la computadora*/
-	CMP r12, #0					@Si r12 esta en 0 lanzara los dados (unicamente al inicio)
-	BEQ lanzar_dados
+	BL ganar
 
-	CMP r11, #0					@Si r11 esta en 0 es turno del jugador1
-	BEQ jugador1
-	BGT computadora
+	CMP r11, #0					@Si r11 esta en 0 lanzara los dados (unicamente al inicio)
+	BLEQ lanzar_dados
 
+	CMP r10, #0					@Si r10 esta en 0 es turno del jugador1
+	BLEQ jugador1
+	CMP r10, #1
+	BLEQ computadora
 
-
-jugador1:
-	
-lanzar_dados:
+	B single_player
 
 multiplayer:
-	B fin
+	/*Jugar contra una persona*/
+	BL ganar
+
+	CMP r11, #0					@Si r11 esta en 0 lanzara los dados (unicamente al inicio)
+	BLEQ lanzar_dados
+
+	CMP r10, #0					@Si r10 esta en 0 es turno del jugador1
+	BLEQ jugador1
+	CMP r10, #1
+	BLEQ jugador2
+
+	B multiplayer
+
+ganar:
+	CMP r7, #10
+	BEQ ganar_jugador1
+
+	CMP r8, #10
+	BEQ ganar_jugador2
+
+	MOV pc, lr
+
+ganar_jugador1:
+	LDR r0, =jugador1_gano
+	BL puts
+	POP {pc}
+
+ganar_jugador2:
+	LDR r0, =jugador2_gano
+	BL puts
+	POP {pc}
+
+jugador1:
+
+	PUSH {lr}
+
+	/*Turno del jugador 1*/
+
+	LDR r0, =turno_jugador1
+	BL puts
+
+	LDR r0, =mensaje_mayor_menor			@Pregunta si el sigiente valor sera mayor o menor
+	BL puts
+
+	LDR r0, =ingreso_valor				@Ingreso de valor
+	LDR r1, =opcion_mayor_menor
+	BL scanf
+
+	CMP r0, #0					@Progra defensiva en caso ingrese mal el dato
+	BEQ error_jugador1
+
+	LDR r1, =opcion_mayor_menor			@Carga opcion ingresada
+	LDR r1, [r1]
+
+	CMP r1, #0					@En caso el jugador haya elegio que el resultado sera menor
+	BLEQ menor
+
+	CMP r1, #1					@En caso el jugador haya elegido que el resultado sera mayo
+	BLEQ mayor
+
+	BGT error_jugador1				@En caso haya ingresado mal el valor
+
+	MOV r10, #1					@Cambio de turno
+
+	POP {pc}
+
+
+jugador2:
+
+	PUSH {lr}
+
+	/*Turno del jugador 2*/
+
+	LDR r0, =turno_jugador2
+	BL puts
+
+	LDR r0, =mensaje_mayor_menor			@Pregunta si el sigiente valor sera mayor o menor
+	BL puts
+
+	LDR r0, =ingreso_valor				@Ingreso de valor
+	LDR r1, =opcion_mayor_menor
+	BL scanf
+
+	CMP r0, #0					@Progra defensiva en caso ingrese mal el dato
+	BEQ error_jugador2
+
+	LDR r1, =opcion_mayor_menor			@Carga opcion ingresada
+	LDR r1, [r1]
+
+	CMP r1, #0					@En caso el jugador haya elegio que el resultado sera menor
+	BLEQ menor
+
+	CMP r1, #1					@En caso el jugador haya elegido que el resultado sera mayo
+	BLEQ mayor
+
+	BGT error_jugador2				@En caso haya ingresado mal el valor
+
+	MOV r10, #0					@Cambio de turno
+
+	POP {pc}
+
+
+computadora:
+	/*Turno de la computadora*/
+
+	PUSH {lr}
+
+	LDR r0, =turno_computadora
+	BL puts
+
+	MOV r12, #2					@Se generara un numero aleatoreo entre 1 y 2
+	BL RANDOM
+	MOV r1, #1
+
+	CMP r1, #1
+	BLEQ menor
+
+	CMP r1, #2
+	BLEQ mayor
+
+	MOV r10, #0
+
+	POP {pc}
+
+menor:
+	PUSH {lr}
+
+	MOV r6, r9					/*R6 Guardara el valor anterior de los dados*/
+
+	BL lanzar_dados					@Lanzar los dados para el nuevo valor
+
+	CMP r9, r6					@En caso el nuevo valor sea menor se da un punto
+	BLT punto
+
+	POP {lr}
+
+	BEQ menor					@En caso sea igual se lanzan nuevamente los dados
+
+	MOV pc, lr
+
+mayor:
+	PUSH {lr}
+
+	MOV r6, r9					/*R6 Guardara el valor anterior de los dados*/
+
+	BL lanzar_dados					@Lanzar los dados para el nuevo valor
+
+	CMP r9, r6
+	BGT punto					@En caso el nuevo valor sea menor se da un punto
+
+	POP {lr}
+
+	BEQ mayor					@En caso sea igual se lanzan nuevamente los dados
+
+	MOV pc, lr
+
+punto:
+	/*Subrutina llamada cuando algun jugador realiza un punto*/
+	CMP r10, #0
+	BEQ punto_jugador1
+	BGT punto_jugador2
+
+punto_jugador1:
+	ADD r7, #1
+	POP {pc}
+
+punto_jugador2:
+	ADD r8, #1
+	POP {pc}
+
+lanzar_dados:
+	/*Lanzar dos dados y obtener numeros aleatoreos*/
+	MOV r11, #1
+
+	PUSH {lr}					@Guarda la instruccion en donde fue llamada la subrutina
+
+	/*Marcador actual*/
+	LDR r0, =mensaje_marcador
+	BL puts
+
+	LDR r0, =marcador_jugador1			@Marcador jugador 1
+	MOV r1, r7
+	BL printf
+
+	LDR r0, =marcador_jugador2			@arcador jugador 2
+	MOV r1, r8
+	BL printf
+
+	/*Primer dado*/
+	MOV r12, #6
+	BL RANDOM					@Llama a la subrutina que genera numeros aleatorios
+	MOV r2, r12					@Asigna el valor aleatorio para luego sumarlo a dado 2
+
+	/*Segundo dado*/
+	MOV r12, #6
+	BL RANDOM
+	MOV r3, r12
+
+	/*Suma dado 1 y dado 2*/
+	MOV r1, #0
+	ADD r1, r2, r3
+
+	MOV r9, r1					/*R9 Contendra la suma de los dados*/
+
+	LDR r0, =mensaje_dados				@Imprime el valor de los dados
+	BL printf
+
+	POP {pc}
 
 error:
+	POP {lr}
+
 	LDR r0, =mensaje_error 				@Mensaje de Error
 	BL puts
 	BL getchar					@Refresca el buffer
+
 	B modo_juego
+
+error_jugador1:
+	LDR r0, =mensaje_error				@Mensaje de error
+	BL puts
+	BL getchar					@Refresca el buffer*/
+
+	POP {lr}
+
+	B jugador1
+
+error_jugador2:
+	LDR r0, =mensaje_error				@Mensaje de error
+	BL puts
+	BL getchar					@Refresca el buffer*/
+
+	POP {lr}
+
+	B jugador2
+
 
 fin:
 	MOV r0, #0
--- random.s
+++ random.s
@@ -1,77 +1,31 @@
-@----------------------------------
+﻿.global	__aeabi_idivmod
+.align	2
+.global	RANDOM
+.type	RANDOM, %function
 
-.data
-
-@ See /usr/include/arm-linux-gnueabihf/asm/unistd.h
-@ See /usr/include/arm-linux-gnueabihf/bits/fcntl-linux.h
-
-    .equ create,     8
-         .equ Mode, 0644       @ -rw-r--r--
-    .equ open,       5
-         .equ Rd,   00
-         .equ Wr,   01
-         .equ RdWr, 02
-         .equ Apnd, 02000
-    .equ read,       3
-    .equ write,      4
-    .equ close,      6
-    .equ sync,       36
-    .equ exit,       1
-    .equ sfile,      187
-
-@----------------------------------
-
-.balign 4
-dir_file:
-    .asciz "/dev/urandom"
-
-.balign 4
-Open:
-    .word dir_file, RdWr | Apnd, open
-
-.balign 4
-Buf:
-    .word 0 
-
-.balign 4
-Read:
-    .word Buf, 1, read
-
-.balign 4
-format:
-    .asciz "%3d\n"
-
-@----------------------------------
-
-.text
-
-.global aleatorios
-
-aleatorios:
-
-@    push   {r4, r5, r7, lr}     folowing AAPCS
-
-     ldr    r3, =Open            @ load address
-     ldm    r3, {r0, r1, r7}     @ load registers
-     svc    #0                   @ OS opens file
-     mov    r4, r0               @ save fd in r4
-
-     ldr    r3, =Read            @ load address
-     ldm    r3, {r1, r2, r7}     @ load registers
-     svc    #0                   @ OS reads file
-
-     mov    r0, r4               @ move fd in r0
-     mov    r7, #close           @ num for close
-     svc    #0                   @ OS closes file
-
-@     ldr    r0, =format          adress of format
-       @ load byte
-@     bl     printf               @ C() print byte
-@     mov    r0, #0               @ 0 = success
-
-exit:
-
-@     pop    {r4, r5, r7, lr}     @ folowing AAPCS
-     ldr    r1, =Buf             @ addr of byte red
-     ldr    r0, [r1]      
-     mov    pc, lr                   @ Exit if use gcc as linker
+@**    R12 <- Al inicio indica el valor máximo de random
+@**    R12 <- Al terminar contiene el valor generado
+RANDOM:
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	
+	PUSH  {FP, LR}
+	
+	ADD   FP, SP, #4
+	SUB   SP, #8
+	STR   R12, [FP, #-8]
+	MOV   R0, #0
+	BL    time
+	MOV   R12, R0
+	MOV   R0, R12
+	BL    srand
+	BL    rand
+	MOV   R12, R0
+	LDR   R1, [FP, #-8]
+	MOV   R0, R12
+	BL    __aeabi_idivmod
+	MOV   R12, R1
+	ADD   R12, R12, #1
+	SUB   SP, FP, #4
+	
+	POP   {FP, PC}
